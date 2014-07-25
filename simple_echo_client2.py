87302a14664efdf7ffcf06759802b40450b7c7eb
Michael Christopher Robinson <mrobinson@heron64.robinson-west.com>
2014-07-24 17:55:43 -0700

    Simple python socket scripts and a text message.

---
+++ simple_echo_client2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# Echo client program
+import socket
+
+HOST = ''    		  # The remote host
+PORT = 50007              # The same port as used by the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+file1 = open('test.txt','r')
+content = file1.read()
+s.send(content)
+data = s.recv(1024)
+s.close()
+file1.close()
+print 'Received', repr(data)
